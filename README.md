62b4451e18783d4bd9daa8a6ba089694709ecf46
Nikolay Nedelko <nikolainedelko777@gmail.com>
2023-01-27 13:16:21 +0600

    edit readme

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+**Check File Exists**
+
+Eng: The function goes through the files in the current folder if the name is in the files and if this file exists and is not equal to !=0 and if the size is less than specified, then True if not, then False
+
+Rus: Функция проходится в текущей папке по файлам если имя есть в файлах и если этот файл существует и не равен !=0 и если размер меньше чем указали то True если нет то False
--- check_file_exists.py
+++ check_file_exists.py
@@ -2,11 +2,14 @@ import os
 
 
 def check_file_exists(curdir, name_file, size_file):
+    # The function goes through the files in the current folder
     for root, dirs, files in os.walk(curdir):
         for file in files:
             print(file)
+            # if the name is in the files and if this file exists and is not equal to !=0
             if name_file in file:
                 if os.path.exists(file) and os.stat(file).st_size != 0:
+                    # if the size is less than specified then True if not then False
                     if os.path.getsize(file) < size_file:
                         return True
                     else:
@@ -15,7 +18,7 @@ def check_file_exists(curdir, name_file, size_file):
 
 curdir = os.path.abspath(os.curdir) + "\\"
 size_file = 3000000
-name_file = "clesh"
+name_file = "ref"
 
 test_var = check_file_exists(curdir, name_file, size_file)
 print(test_var)
